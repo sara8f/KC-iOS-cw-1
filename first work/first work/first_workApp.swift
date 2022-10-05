23c02701ac9056df6147a1b83acaecd562c74b0c
sara8f <114738785+sara8f@users.noreply.github.com>
2022-10-05 19:06:27 +0300

    done

---
+++ first work/first work/first_workApp.swift
@@ -0,0 +1,17 @@
+//
+//  first_workApp.swift
+//  first work
+//
+//  Created by user on 05/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct first_workApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
